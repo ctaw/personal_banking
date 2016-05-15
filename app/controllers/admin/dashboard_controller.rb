f923e3eb6f902b7f11b5f786e425cd78266ad767
Charlene Lopez Taw <tawcharlene@gmail.com>
2016-05-16 04:04:50 +0800

    header update

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -6,13 +6,17 @@ class Admin::DashboardController < AdminController
     @credit_reminders = Credit.where(:reminders_date => DateTime.now.to_date).count
     @credits = Credit.select("id, customer_name, transaction_type_id, amount").order("id DESC").paginate(:page => params[:credits], :per_page => 5)
     @debits = Debit.select("id, customer_name, transaction_type_id, amount").order("id DESC").paginate(:page => params[:credits], :per_page => 5)
-		@credit = Credit.new
+    @credit_sum = Credit.sum(:amount)
+    @debit_sum = Debit.sum(:amount)
+    @credit = Credit.new
     @debit = Debit.new
 	end
 
   def search
     @credit_customers = Credit.search(params[:search]).order("id DESC").paginate(:page => params[:credit_customers], :per_page => 5)
     @debit_customers = Debit.search(params[:search]).order("id DESC").paginate(:page => params[:debit_customers], :per_page => 5)
+    @customer_credit_total = Credit.search(params[:search]).sum(:amount)
+    @customer_debit_total = Debit.search(params[:search]).sum(:amount)
   end
 
   def date_range
@@ -21,7 +25,10 @@ class Admin::DashboardController < AdminController
 
     @all_credit_customers = Credit.where(:transaction_date => @start_date.to_datetime.beginning_of_day..@end_date.to_datetime.end_of_day).order("id DESC").paginate(:page => params[:all_credit_customers], :per_page => 5)
     @all_debit_customers = Debit.where(:transaction_date => @start_date.to_datetime.beginning_of_day..@end_date.to_datetime.end_of_day).order("id DESC").paginate(:page => params[:all_debit_customers], :per_page => 5)
+    @total_credit = Credit.where(:transaction_date => @start_date.to_datetime.beginning_of_day..@end_date.to_datetime.end_of_day).sum(:amount)
+    @total_debit = Debit.where(:transaction_date => @start_date.to_datetime.beginning_of_day..@end_date.to_datetime.end_of_day).sum(:amount)
   end
+
 
 	private
 
